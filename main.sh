dd97fed3ef6f9c1b1aeb05c012456d20c0b87c60
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2022-08-22 09:01:41 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@
 git config --global user.name "$GH_USERNAME"
 git config --global user.email "$GH_EMAIL"
 
-git clone -j136 --single-branch "https://fadlyas07:$GH_TOKEN@github.com/greenforce-project/llvm-project/" -b main $(pwd)/llvm-project
+git clone -j136 --single-branch "https://timangpopi1:$GH_TOKEN@github.com/greenforce-project/llvm-project/" -b main $(pwd)/llvm-project
 while true; do
     cd $(pwd)/llvm-project
     git pull https://github.com/llvm/llvm-project main
